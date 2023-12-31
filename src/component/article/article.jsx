d1d890705cfcd510ed52f8f1fc26e060e6240228
Sedanah Mashhour <mashhour.h@northeastern.edu>
2023-12-31 13:39:33 -0500

    help

---
+++ src/component/article/article.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './article.css';
+
+export const article = () => {
+  return (
+    <div>article</div>
+  )
+}
+
+export default article
---
+++ src/containers/possibility/Possibility.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './possibility.css';
+
+const Possibility = () => {
+    return (
+        <div>
+        <h1>Possibility</h1>Possibility</div>
+    )
+    }
+
+export default Possibility
